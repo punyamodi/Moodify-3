931579355aa0145c899676d07eea974d87a7b654
Punya Modi <modipunya@gmail.com>
2024-10-20 18:32:19 +0530

    Fix dependencies and update package-lock.json

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Moodify-3/',  // Replace 'Moodify-3' with your repository name
+  base: '/Moodify-3/',  // Add this line with your repository name
 })
