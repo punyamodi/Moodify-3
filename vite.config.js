424dbb7f111e3ef1f83ffc327dd1a15c4e3f2d67
Punya Modi <modipunya@gmail.com>
2024-10-20 18:46:49 +0530

    Setup for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Moodify-3/',  // Add this line with your repository name
+  base: '/Moodify-3/',  // Add this line and replace with your repository name
 })
