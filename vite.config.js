d3ee34468e72afb3a6d1914844bc5830fadedbb5
Punya Modi <modipunya@gmail.com>
2024-10-20 18:13:39 +0530

    Added GitHub Actions workflow for deploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Moodify-3/',  // Replace 'Moodify-3' with your repository name
 })
